094a6e65606ef82801352f33d16fe7fabacc67e7
Night <soren913@gmail.com>
2026-09-15 17:35:27 +0200

    Added featured news to display 3 random news on frontpage

--- Gratissimo/src/pages/FrontPage.vue
+++ Gratissimo/src/pages/FrontPage.vue
@@ -1,6 +1,8 @@
 <template>
+  <FeaturedNews />
   <TestimonivesSlider />
 </template>
 <script setup>
+import FeaturedNews from '@/components/FeaturedNews.vue'
 import TestimonivesSlider from '@/components/TestimonivesSlider.vue'
 </script>
