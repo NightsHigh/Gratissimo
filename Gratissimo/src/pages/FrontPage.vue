f6083f1342c96e6f465c610e5c6483bebcd44d94
Night <soren913@gmail.com>
2026-09-15 12:55:30 +0200

    Add space-7 space-8 to the otkens.scss

--- Gratissimo/src/pages/FrontPage.vue
+++ Gratissimo/src/pages/FrontPage.vue
@@ -1,7 +1,9 @@
 <template>
+  <Header/>
   <h1>Forside</h1>
   <TestimonivesSlider />
 </template>
 <script setup>
 import TestimonivesSlider from '@/components/TestimonivesSlider.vue'
+import Header from '@/components/Header.vue'
 </script>
