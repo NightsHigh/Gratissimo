bad467e71ddedbaabf90f4ea6b6fcb2a3952d3f5
Night <soren913@gmail.com>
2026-09-15 13:15:33 +0200

    Added CTA for the login below the navbar but above the main content of the page

---
+++ Gratissimo/src/components/CTA.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="cta">
+        <p>Vi hjælper dig på vej til dit næste frivillige job</p>
+    <a href="/login">
+        <button class="cta__button">Log in eller opret dig</button>
+    </a>
+</section>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped lang="scss">
+.cta {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    font-size: var(--font-size-l);
+    padding-top: var(--space-2);
+    padding-bottom: var(--space-2);
+    padding: var(--space-2) var(--space-6);
+    &__button {
+        text-decoration: none;
+        border-radius: var(--radius-pill);
+        background-color: var(--color-accent-dark);
+        color: var(--color-accent-text);
+        padding: var(--space-2) var(--space-6);
+        font-size: var(--font-size-l);
+        border: none;
+    }
+}
+</style>
