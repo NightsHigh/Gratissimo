2b30a889cd47f4b028cfb9f5a3a90b335caaaeba
Night <soren913@gmail.com>
2026-09-17 16:51:48 +0200

    Added navigation guard to prevent people who havent logged in from accesing min-side

--- Gratissimo/src/router.js
+++ Gratissimo/src/router.js
@@ -6,6 +6,7 @@ import CreateListingPage from './pages/CreateListingPage.vue'
 import AuthPage from './pages/AuthPage.vue'
 import MyPage from './pages/MyPage.vue'
 import NotFoundPage from './pages/NotFoundPage.vue'
+import { isLoggedIn } from './auth'
 
 export default createRouter({
   history: createWebHistory(),
@@ -17,11 +18,28 @@ export default createRouter({
     { path: '/jobs/:id', component: JobSearchPage },
     { path: '/nyheder', component: NewsPage },
     { path: '/nyheder/:id', component: NewsPage},
-    { path: '/opret-annonce', component: CreateListingPage },
     { path: '/log-ind', component: AuthPage},
     { path: '/opret-profil', component: AuthPage },
-    { path: '/min-side', component: MyPage },
-    { path: '/min-side/favoritter', component: MyPage },
+
+    // Add vue router meta tag with requiresAuth so we can prevent people from accesing it
+    // If they arent logged in
+    { meta: { requiresAuth: true }, path: '/min-side', component: MyPage },
+    { meta: { requiresAuth: true }, path: '/min-side/favoritter', component: MyPage },
+    { meta: { requiresAuth: true }, path: '/opret-annonce', component: CreateListingPage },
+
     { path: '/:pathMatch(.*)*', component: NotFoundPage }
   ]
+})
+
+router.beforeEach((to) => {
+  if (
+    // Check if the path to the site with requiresAuth aka the only site "min side"
+    // if the user goes there we add a guard to check if they are logged in
+    to.meta.requiresAuth &&
+    // and the user isnt logged in
+    !isLoggedIn.value
+  ) {
+    // send brugeren til login
+    return { path: '/log-ind' }
+  }
 })
