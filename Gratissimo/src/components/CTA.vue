7b1c437def5721335759dd2d85fd1b3ec154e9d1
NightsHigh <soren913@gmail.com>
2026-09-18 12:37:08 +0200

    Add more white space above+below CTA and increse font-size of the text (#107)
    Add more white space above+below CTA and increse font-size of the text

--- Gratissimo/src/components/CTA.vue
+++ Gratissimo/src/components/CTA.vue
@@ -17,16 +17,19 @@ import { isLoggedIn } from '@/auth'
     display: flex;
     align-items: center;
     justify-content: space-around;
-    font-size: var(--font-size-l);
+    font-size: var(--font-size-lg);
+    font-weight: 200;
     padding-top: var(--space-2);
     padding-bottom: var(--space-2);
     padding: var(--space-2) var(--space-6);
+    margin: var(--space-3) 0rem;
+    
     &__button {
         text-decoration: none;
         border-radius: var(--radius-pill);
         background-color: var(--color-accent-dark);
         color: var(--color-accent-text);
-        padding: var(--space-2) var(--space-6);
+        padding: var(--space-2) var(--space-8);
         font-size: var(--font-size-l);
         border: none;
         cursor: pointer;
